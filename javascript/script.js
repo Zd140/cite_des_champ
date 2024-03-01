37e8f17f8282b5b55c2a93bd8261031d1bb7fbe9
ZINGWA-DELANEY <delaneyzingwa@gmail.com>
2024-03-01 05:11:47 +0000

    second commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+//hamburger menu
 const menuBtn  = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuRtnIcon = menuBtn.querySelector("i");
@@ -12,4 +13,33 @@ menuBtn.addEventListener("click", ()=>{
 navLinks.addEventListener("click", () => {
     navLinks.classList.remove("open");
     menuRtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+
+const scrollRevealOption = {
+    distance : "50px",
+    origin: "bottom",
+    duration: "1000"
+
+};
+
+//header container
+ScrollReveal().reveal(".header__container p",{
+    ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__container h1",{
+    ...scrollRevealOption,
+    delay:500,
+});
+
+//about container
+
+ScrollReveal().reveal(".about__image img",{
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".about__image .section__sub",{
+    ...scrollRevealOption,
+    delay:500,
 });
