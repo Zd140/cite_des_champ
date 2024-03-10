420c319386a29d33521c6a4088c7a30e800bd03b
ZINGWA-DELANEY <delaneyzingwa@gmail.com>
2024-03-10 21:19:52 +0000

    recent updates

--- javascript/script.js
+++ javascript/script.js
@@ -39,7 +39,27 @@ ScrollReveal().reveal(".about__image img",{
     origin: "left",
 });
 
-ScrollReveal().reveal(".about__image .section__sub",{
+ScrollReveal().reveal(".about__content .section__subheader",{
     ...scrollRevealOption,
     delay:500,
+});
+
+ScrollReveal().reveal(".about__content .section__header",{
+    ...scrollRevealOption,
+    delay:1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description",{
+    ...scrollRevealOption,
+    delay:1500,
+});
+
+ScrollReveal().reveal(".about__btn",{
+    ...scrollRevealOption,
+    delay:2000,
+});
+//room container
+ScrollReveal().reveal(".room__card",{
+    ...scrollRevealOption,
+    interval:500,
 });
